94db4af4b4d66459b858c2cf1b6d4b57b3444d27
Vladislav <vladgm233223@gmail.com>
2023-07-18 21:21:35 +0300

    styles for search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -24,11 +24,13 @@ export default {
                             `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.search}.json?access_token=${import.meta.env.VITE_API_MAP}&types=place`
                         );
                         this.searchResult = JSON.stringify(result.data.features)
-                        console.log(JSON.parse(this.searchResult))
                     } catch (err) {
                         this.searchError = err.message
                         console.log(err)
                     }
+                }
+                else {
+                    this.searchResult = []
                 }
             }, 1000)
 
@@ -41,18 +43,15 @@ export default {
         <div class="search-wrapper">
 
             <input class="search-input" v-model="search" @input="getSearchResult()" type="text"
-                placeholder="Search your city">
+                placeholder="Search your city" />
+            <p v-if="searchError" class="error-message">Something went wrong. <br /> Error: {{ searchError }}</p>
+            <p v-if="searchResult.length < 1" class="error-message">I don't know such a place, try to enter a
+                valid place</p>
             <div class="search-results">
                 <ul class="search-result-list">
-                    <p v-if="searchError" class="error-message">Something went wrong. Error: {{ searchError }}</p>
-                    <p v-else-if="searchResult.length < 1" class="error-message">I don't know such a place, try to enter a
-                        valid place</p>
-                    <li v-else v-for="city in JSON.parse(searchResult)" :key="city.id" class="search-result-item" @click="">
+                    <li v-if="searchResult.length > 1" v-for="city in JSON.parse(searchResult)" :key="city.id" @click="">
                         {{ city.place_name }}
                     </li>
-
-
-
                 </ul>
             </div>
 
