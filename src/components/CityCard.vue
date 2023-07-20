d657d07342ade471d739f3f6c1629d28bb5e7d5c
Vladislav <vladgm233223@gmail.com>
2023-07-20 12:27:30 +0300

    style for hourly data

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -3,7 +3,6 @@ export default {
     data() {
         return {
             currentCityInfo: this.city.list[0]
-
         }
     },
     props: {
@@ -13,9 +12,16 @@ export default {
     },
     computed: {
         hourlyData() {
-            return JSON.parse(JSON.stringify(this.city.list))
-        }
+            return JSON.parse(JSON.stringify(this.city.list)).splice(0, 9)
+        },
     },
+    methods: {
+        timeFormat(dt_txt) {
+            const time = new Date(dt_txt)
+            return `${time.getHours()}:${time.getMinutes()}0`
+
+        }
+    }
 
 }
 </script>
@@ -33,16 +39,18 @@ export default {
         </div>
     </div>
     <div class="hourly-weather">
-        <div class="hourly-weather-item" v-for="weather in hourlyData.splice(0, 9)">
-
-            <p class="time">{{ weather.dt_txt }}</p>
-            <img class="weather-img" :src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
-                " alt="weather-icon" />
-            <p class="description">{{ weather.weather[0].description }}</p>
-            <p class="temp">{{ Math.trunc(weather.main.temp) }}°C</p>
-
-
-
+        <h2 class="hourly-weather-title">Hourly weather</h2>
+        <hr />
+        <div class="wrapper">
+            <div class="hourly-weather-list">
+                <div class="hourly-weather-item" v-for="weather in hourlyData">
+                    <p class="time">{{ timeFormat(weather.dt_txt) }}</p>
+                    <img class="weather-img" :src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+                        " alt="weather-icon" />
+                    <p class="description">{{ weather.weather[0].description }}</p>
+                    <p class="temp">{{ Math.trunc(weather.main.temp) }}°C</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
